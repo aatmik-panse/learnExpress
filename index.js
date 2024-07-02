b1a24d9d673bcc6a3289c57d723d0faeeb17970a
aatmik-panse <aatmik.23bcs10082@ms.sst.scaler.com>
2024-07-02 15:34:23 +0530

    post and put request in express

--- index.js
+++ index.js
@@ -5,16 +5,76 @@ const file = fs.readFileSync("./contact.json");
 
 const app = express();
 
+app.use(express.json());
+
+let admin = [
+  {
+    id: 1,
+    name: "admin",
+    password: "admin",
+    username: "admin",
+  },
+  {
+    id: 2,
+    name: "user",
+    password: "user",
+    username: "user",
+  },
+  {
+    id: 3,
+    name: "user1",
+    password: "user1",
+    username: "user1",
+  },
+];
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 app.get("/about", (req, res) => {
   res.send("About Page");
 });
-
+app.get("/admin", (req, res) => {
+  res.send(admin);
+});
 app.get("/contact", (req, res) => {
   res.send(JSON.parse(file));
+  console.log(JSON.parse(file));
 });
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
+});
+
+app.post("/contact", (req, res) => {
+  res.send("Contact Page");
+  console.log(req.body);
+});
+
+app.put("/contact", (req, res) => {
+  res.send("Contact Page");
+  console.log(req.body);
+});
+
+app.post("/admin", (req, res) => {
+  console.log(req.body);
+  let singleAdmin = {
+    id: admin.length + 1,
+    name: req.body.name,
+    password: req.body.password,
+    username: req.body.username,
+  };
+  admin.push(singleAdmin);
+  res.send(admin);
+});
+
+app.put("/admin", (req, res) => {
+  console.log(req.body);
+  let singleAdmin = {
+    id: req.body.id,
+    name: req.body.name,
+    password: req.body.password,
+    username: req.body.username,
+  };
+  admin.push(singleAdmin);
+  res.send(admin);
 });
